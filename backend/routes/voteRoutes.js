57c3123451dc0a807a61bd5dd17dc2dd1096d496
Jemin Aghera <jemin.aghera28@gmail.com>
2025-09-21 07:33:28 +1000

    Vote Withdrawal feature implemented

--- backend/routes/voteRoutes.js
+++ backend/routes/voteRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { castVote, viewMyVote, updateVote } = require("../controllers/voteController");
+const { castVote, viewMyVote, updateVote, withdrawVote } = require("../controllers/voteController");
 const authMiddleware = require("../middleware/authMiddleware");
 const loggerMiddleware = require("../middleware/loggerMiddleware");
 
@@ -9,4 +9,5 @@ router.post("/", loggerMiddleware, authMiddleware, castVote);
 
 router.get("/", loggerMiddleware, authMiddleware, viewMyVote); // View my vote
 router.patch("/", loggerMiddleware, authMiddleware, updateVote); // Change my vote
+router.delete("/", loggerMiddleware, authMiddleware, withdrawVote); // Withdraw my vote
 module.exports = router;
