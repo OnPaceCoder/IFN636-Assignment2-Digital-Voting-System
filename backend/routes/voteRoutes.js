4ad8871a423d6ddc3098c979634009ce364d6ed4
Pragati <Pragatisidpara@gmail.com>
2025-09-24 23:21:43 +1000

    MyVote page created in frontend

--- backend/routes/voteRoutes.js
+++ backend/routes/voteRoutes.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { castVote, viewMyVote, updateVote, withdrawVote } = require("../controllers/voteController");
+const { castVote, viewMyVote, updateVote, withdrawVote, getVoteStatus } = require("../controllers/voteController");
 const authMiddleware = require("../middleware/authMiddleware");
 const loggerMiddleware = require("../middleware/loggerMiddleware");
 
 // Protected route: only authenticated users can cast votes
 router.post("/", loggerMiddleware, authMiddleware, castVote);
-
+router.get("/status", loggerMiddleware, authMiddleware, getVoteStatus)
 router.get("/", loggerMiddleware, authMiddleware, viewMyVote); // View my vote
 router.patch("/", loggerMiddleware, authMiddleware, updateVote); // Change my vote
 router.delete("/", loggerMiddleware, authMiddleware, withdrawVote); // Withdraw my vote
