1a5e637ecccbf7a69b7ff7c1d5e9ef728c6c0cc4
Priyank Vasoya <priyankvasoya7@gmail.com>
2025-09-29 06:49:58 +1000

    Public IP added and replaced with localhost

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001', // local
-  // baseURL: 'http://3.107.31.165:5001', // live
+  // baseURL: 'http://localhost:5001', // local
+  baseURL: 'http://3.107.161.64:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
